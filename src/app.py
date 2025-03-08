7a87ac8db84e405a41c6f1405ad5ffc1d412d3f2
1122gs <GORDON.SMITHJR@hotmail.com>
2025-03-08 00:39:13 +0000

    final commit

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Planet, Favorite
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,14 +28,19 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
@@ -44,6 +50,86 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    return jsonify(people)
+
+
+@app.route('/people/<int:id>')
+def get_one_person(id):
+    people = People.query.get(id)
+    return jsonify(people)
+
+
+@app.route('/planet')
+def get_planets():
+    planet = Planet.query.all()
+    return jsonify(planet)
+
+
+@app.route('/planet/<int:id>')
+def get_one_planet(id):
+    get_planet = Planet.query.get(id)
+    return jsonify(get_planet)
+
+
+@app.route('/users')
+def get_user():
+    this_user = User.query.all()
+    return jsonify(this_user)
+
+
+@app.route('/users/favorite')
+def get_user_favorite():
+    user_id = Favorite().user_favorite
+    favorites = Favorite.query.filter_by(user_id=user_id).all()
+    return jsonify(favorites)
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    user_id = request.json.get("user_id")
+    new_favorite = Favorite(user_id=user_id, planet_id=planet_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+    
+    return jsonify(new_favorite.serialize()), 201
+
+
+@app.route('/favorite/people/<int:id>', methods=["POST"])
+def add_people_favorite(id):
+     user_id = request.json.get()
+     new_favorite = Favorite(user_id=user_id, people_id=id)     
+     db.session.add(new_favorite)
+     db.session.commit()
+
+     return jsonify({"message": "Favorite added successfully", "favorite": {
+        "id": new_favorite.id,
+        "people_favorite": new_favorite.people_favorite,
+    }}), 201
+
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def remove_favorite_planet(planet_id):
+    user_id = request.json.get("user_id")
+    favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+    db.session.delete(favorite)
+    db.session.commit()
+    return jsonify({"message": "Favorite planet removed"}), 200
+
+
+
+@app.route('/favorite/people/<int:id>', methods=["DELETE"])
+def delete_people_favorite(people_id):
+    user_id = request.json.get("user_id")
+    people_fav = Favorite.query.filter_by(user_id=user_id, people_favorite=people_id).first
+    db.session.delete(people_fav)
+    db.session.commit()
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
@@ -17,3 +17,61 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class People(db.Model):
+    __tablename__ = "people"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    height: Mapped[str] = mapped_column(nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(), nullable=False)
+    planet: Mapped[str] = mapped_column(String())
+    eye_color: Mapped[str] = mapped_column()
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "planet":self.planet,
+            "height": self.height
+            # do not serialize the password, its a security breach
+        }
+
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_favorite : Mapped[int] = mapped_column(ForeignKey("user.id"))
+    planet_favorite: Mapped[int] = mapped_column(ForeignKey("planet.id"))
+    people_favorite: Mapped[int] = mapped_column(ForeignKey('people.id'))
+
+    def serialize(self):
+         return{
+              "id":self.id,
+              "user_favorite": self.user_favorite,
+              "planet_favorite": self.planet_favorite,
+              "people_favorite": self.people_favorite
+
+         }
+
+
+class Planet(db.Model):
+        __tablename__ = 'planet'
+        id: Mapped[int] = mapped_column(primary_key=True)
+        population: Mapped[str] = mapped_column(nullable=False)
+        size: Mapped[str] = mapped_column(nullable=False)
+        gas: Mapped[str]
+
+        def serialize(self):
+         return{
+              "id":self.id,
+              "population ": self.population,
+              "size": self.size,
+              "gas": self.gas
+
+         }
+
